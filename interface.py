90dd246c9150752408e2ec0082b940e96c9485d1
chm626 <chm626@lehigh.edu>
2025-10-21 17:47:20 -0400

    eh

--- interface.py
+++ interface.py
@@ -1,19 +1,18 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
+import streamlit
+import pandas
+import numpy
 
-st.write("Streamlit supports a wide range of data visualizations, including [Plotly, Altair, and Bokeh charts](https://docs.streamlit.io/develop/api-reference/charts). 📊 And with over 20 input widgets, you can easily make your data interactive!")
+streamlit.write("Streamlit supports a wide range of data visualizations, including [Plotly, Altair, and Bokeh charts](https://docs.streamlit.io/develop/api-reference/charts). 📊 And with over 20 input widgets, you can easily make your data interactive!")
 
-all_users = ["Alice", "Bob", "Charly"]
-with st.container(border=True):
-    users = st.multiselect("Users", all_users, default=all_users)
-    rolling_average = st.toggle("Rolling average")
+all_users = ["Alice", "Bob", "Charlie"]
+with streamlit.container(border=True):
+    users = streamlit.multiselect("Users", all_users, default=all_users)
+    rolling_average = streamlit.toggle("Rolling average")
 
-np.random.seed(42)
-data = pd.DataFrame(np.random.randn(20, len(users)), columns=users)
+numpy.random.seed(42)
+data = pandas.DataFrame(numpy.random.randn(20, len(users)), columns=users)
 if rolling_average:
     data = data.rolling(7).mean().dropna()
 
-tab1, tab2 = st.tabs(["Chart", "Dataframe"])
+tab1, tab2 = streamlit.tabs(["Chart", "Dataframe"])
 tab1.line_chart(data, height=250)
-tab2.dataframe(data, height=250, use_container_width=True)
